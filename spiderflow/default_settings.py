a1051ce5bcf19d5d63fb59a21679a3502a3a3d98
zzpwelkin <user.name>
2015-02-15 15:36:41 +0800

    [REV] Revise default log dict

--- spiderflow/default_settings.py
+++ spiderflow/default_settings.py
@@ -4,41 +4,52 @@
 default settings
 """
 LOGGER = {
-                'version':1,
-                'handlers':
-                {
-                                'console':
-                                {
-                                                'class':'logging.StreamHandler', 
-                                                 'level'   : 'INFO',
-                                                 'stream'  : 'ext://sys.stdout'
-                                                }
-                                },
-                                'loggers':
-                                {
-                                                'spider':
-                                                {
-                                                                'handlers':['console',],
-                                                                'propagate':True,
-                                                                'level':'INFO',
-                                                                },
-                                                 'spiderprocess':
-                                                {
-                                                                'handlers':['console',],
-                                                                'propagate':True,
-                                                                'level':'INFO',
-                                                                },
-                                                 'process':
-                                                {
-                                                                'handlers':['console',],
-                                                                'propagate':True,
-                                                                'level':'INFO',
-                                                                },
-                                                 'storage':
-                                                {
-                                                                'handlers':['console',],
-                                                                'propagate':True,
-                                                                'level':'INFO',
-                                                                },
-                                                },
+        'version':1,
+        'formatters': {
+            'detail':{
+                'format':'%(name)s %(levelname)s %(pathname)s %(process)d %(thread)d %(message)s\n',
+                'datefmt':'%Y-%m-%d %H:%M:%S',
+                },
+            'simple':{
+                'format':'%(name)s %(levelname)s: %(message)s\n',
+                'datefmt':'%Y-%m-%d %H:%M:%S',
+                },
+            },
+        'handlers':
+        {
+            'console':
+            {
+                'class':'logging.StreamHandler', 
+                'level'   : 'INFO',
+                'stream'  : 'ext://sys.stdout',
+                'formatter': 'simple', 
                 }
+            },
+        'loggers':
+        {
+            'spider':
+            {
+                'handlers':['console',],
+                'propagate':True,
+                'level':'INFO',
+                },
+            'spiderprocess':
+            {
+                'handlers':['console',],
+                'propagate':True,
+                'level':'INFO',
+                },
+            'process':
+            {
+                'handlers':['console',],
+                'propagate':True,
+                'level':'INFO',
+                },
+            'storage':
+            {
+                'handlers':['console',],
+                'propagate':True,
+                'level':'INFO',
+                },
+            },
+        }
