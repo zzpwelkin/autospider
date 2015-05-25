1dfa73625735dcbae90f61e988de7843f3563b2d
zzpwelkin <user.name>
2015-05-25 11:31:55 +0800

    [REV] User custom define the driver of database that will be used

--- spiderflow/storage/rdbstorage.py
+++ spiderflow/storage/rdbstorage.py
@@ -4,8 +4,8 @@ from . import StorageBase
 class RDBStorage(StorageBase):
     
     def __init__(self, host='localhost', port=3306, username='', 
-                    password='', database='', charset='utf8', verbose=False, **kwargs):
-        self.engine = create_engine("mysql://{u}:{p}@{h}:{prt}/{db}".format(
+                    password='', database='', charset='utf8', verbose=False, driver='mysql', **kwargs):
+        self.engine = create_engine("{dr}://{u}:{p}@{h}:{prt}/{db}".format(dr=driver,
                         u=username, p=password, h=host, prt=port, db=database),
                         encoding=charset, echo=verbose)
         self.metadata = MetaData()
